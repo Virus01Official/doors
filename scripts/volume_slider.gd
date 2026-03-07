8a56c9e559e5545dcbd87d208e19c7aa11b7876d
Virus01Official <virusidkoff@gmail.com>
2026-03-07 13:52:34 +0100

    volume slider works

--- scripts/volume_slider.gd
+++ scripts/volume_slider.gd
@@ -2,4 +2,5 @@ extends HSlider
 
 func _on_value_changed(valued: float) -> void:
 	$"../../../../roll".play()
-	data.Volume = valued
+	var db = linear_to_db(valued / 100.0)
+	AudioServer.set_bus_volume_db(0, db)
